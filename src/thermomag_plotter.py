6959a781f2ded0774b527d499cd3626f5a0d302b
Katie Bristol <kebristo@mtu.edu>
2021-03-24 16:04:07 -0400

    Breaking my code even more

--- src/thermomag_plotter.py
+++ src/thermomag_plotter.py
@@ -17,15 +17,15 @@ lt_data = read_data("data/DA7_1-L1.csv")
 ht_data = read_data("data/DA7_1-H1.csv")
 lt2_data = read_data("data/DA7_1-L2.csv")
 
-def convert(filename, delimiter=',', starting_row=0):
+#def convert(filename, delimiter=',', starting_row=0):
         """This function converts the temperature data and
         appends the processed data onto each dataframe."""
         # Convert from celsius to kelvin
-        conversion = (filename[:,0,None] + 273.15)
+ #       conversion = (filename[:,0,None] + 273.15)
 
         # Append the converted data to the existing dataframe.
-        processed_data = np.append(all_data, conversion,1)
-        return processed_data
+#        processed_data = np.append(all_data, conversion,1)
+#        return processed_data
 
 # I think I can delete all of this now but may want to double check... 
         #raw_lt_data = np.genfromtxt("data/DA7_1-L1.csv", delimiter=',', skip_header=0)
@@ -38,61 +38,36 @@ def convert(filename, delimiter=',', starting_row=0):
         #lt2_data = np.array(raw_lt2_data[1:,:], dtype=float)
 
 # Convert temperature units from celsius to kelvin
-#lt_conversion = (lt_data[:,0,None] + 273.15)
-#ht_conversion = (ht_data[:,0,None] + 273.15)
-#lt2_conversion = (lt2_data[:,0,None] + 273.15)
+lt_conversion = (lt_data[:,0,None] + 273.15)
+ht_conversion = (ht_data[:,0,None] + 273.15)
+lt2_conversion = (lt2_data[:,0,None] + 273.15)
 
 # Append these new columns to the existing data arrays
-#processed_lt_data = np.append(lt_data, lt_conversion,1)
-#processed_ht_data = np.append(ht_data, ht_conversion,1)
-#processed_lt2_data = np.append(lt2_data, lt2_conversion,1)
-#print (processed_lt_data[1])
+processed_lt_data = np.append(lt_data, lt_conversion,1)
+processed_ht_data = np.append(ht_data, ht_conversion,1)
+processed_lt2_data = np.append(lt2_data, lt2_conversion,1)
+print (processed_lt_data[1])
 
 # Create a figure of the processed data
-def plot(filename, delimiter=',', starting_row=0):
-        """This function reads data from a specified filename.
-        The specified filename should point to a .csv file."""
-        # Create an array (a multi-dimensional table) out of our data file, full of text
-        thermomag_figure = plt.figure()
 
-        plt.plot(processed_data[:,9],
-        processed_data[:,2],
-        color='blue', 
-        linestyle='dashed', 
-        linewidth='1.5', 
-        label="L1")
-
-        titlefont = {'family': 'serif',
-             'color': 'black',
-             'weight': 'bold',
-             'size': 14}
-        axesfont = {'family': 'sans-serif',
-            'style': 'italic',
-            'color': 'darkslategray',
-            'weight': 'normal',
-            'size': 12}
-
-        plt.show(block=True)
-        thermomag_figure.savefig('results/thermomag-plot.png')
-
-#thermomag_figure = plt.figure()
+thermomag_figure = plt.figure()
 
 ## Plotting each dataset as a line
-#plt.plot(processed_lt_data[:,9],
+plt.plot(processed_lt_data[:,9],
         processed_lt_data[:,2],
         color='blue', 
         linestyle='dashed', 
         linewidth='1.5', 
         label="L1")
 
-#plt.plot(processed_ht_data[:,9],
+plt.plot(processed_ht_data[:,9],
          processed_ht_data[:,2],
          color='firebrick', 
          linestyle='solid', 
          linewidth='1.5', 
          label="H1")
 
-#plt.plot(processed_lt2_data[:,9],
+plt.plot(processed_lt2_data[:,9],
          processed_lt2_data[:,2],
          color='blue', 
          linestyle='dotted',
@@ -100,21 +75,21 @@ def plot(filename, delimiter=',', starting_row=0):
          label="L2")
 
 ## Adding arrows to indicate direction of measurement
-#plt.arrow(522,2280, 100, 0, head_width=20, head_length=18, fc='k', ec='k')
-#plt.arrow(852,2119, 15, -150, head_width=18, head_length=22, fc='k', ec='k')
-#plt.arrow(866,1196, -20, 150, head_width=18, head_length=22, fc='k', ec='k')
-#plt.arrow(444,1858, -55, 150, head_width=18, head_length=22, fc='k', ec='k')
+plt.arrow(522,2280, 100, 0, head_width=20, head_length=18, fc='k', ec='k')
+plt.arrow(852,2119, 15, -150, head_width=18, head_length=22, fc='k', ec='k')
+plt.arrow(866,1196, -20, 150, head_width=18, head_length=22, fc='k', ec='k')
+plt.arrow(444,1858, -55, 150, head_width=18, head_length=22, fc='k', ec='k')
 
 ## Specifying parameters for fonts
-#titlefont = {'family': 'serif',
-#             'color': 'black',
-#             'weight': 'bold',
-#             'size': 14}
-#axesfont = {'family': 'sans-serif',
-#            'style': 'italic',
-#            'color': 'darkslategray',
-#            'weight': 'normal',
-#            'size': 12}
+titlefont = {'family': 'serif',
+             'color': 'black',
+             'weight': 'bold',
+             'size': 14}
+axesfont = {'family': 'sans-serif',
+            'style': 'italic',
+            'color': 'darkslategray',
+            'weight': 'normal',
+            'size': 12}
 
 ## Adding title, axes labels, and legend
 plt.title('Thermomagnetic Behavior', fontdict=titlefont)
