364484b2fdde3c660811206bc13aada6c82f6dbd
Katie Bristol <kebristo@mtu.edu>
2021-03-27 17:22:32 -0400

    Fixed functions and removed print commands

--- src/thermomag_plotter.py
+++ src/thermomag_plotter.py
@@ -20,23 +20,20 @@ ht_data = read_data("data/DA7_1-H1.csv")
 lt2_data = read_data("data/DA7_1-L2.csv")
 
 def process_data(data_array):
+    """This function processes the data by converting temperature 
+    from units of Celsius to Kelvin."""
     # Compute a new column by multiplying column number 1 to Kelvin
     temp_kelvin = (data_array[:,0,None] + 273.15)
 
-    # Append this new column to the existing temperature_data array
+    # Append this new column to the existing data array
     processed_data = np.append(data_array, temp_kelvin,1)
     return processed_data
 
 processed_lt_data = process_data(lt_data)
 processed_ht_data = process_data(ht_data)
 processed_lt2_data = process_data(lt2_data)
-print(processed_ht_data)
 
-# Create a figure of the processed data
-## I tried everything I could think of to make this a function but nothing worked
-## My issue seems that each dataset (lt, ht, lt2) require different parameters for plotting so one function wouldn't work.
-## I tried an if statement to at least separate the lt's from ht but could not get all three to plot on one graph
-## without it yelling at me about "unexpected" indents. I need to get gud. 
+# Create a figure of the processed data 
 thermomag_figure = plt.figure()
 
 ## Plotting each dataset as a line with unique colors and line styles
@@ -90,20 +87,9 @@ plt.legend()
 plt.show(block=True)
 thermomag_figure.savefig('results/thermomag-plot.png')
 
-# Converting the data to pandas dataframes and then outputting as .json format
-## Maybe this can be a function, try later 
-#lt_data_pd = pd.read_csv("data/DA7_1-L1.csv", header=0)
-#lt_data_pd.to_json("results/lt_output.json")
-#ht_data_pd = pd.read_csv("data/DA7_1-h1.csv", header=0)
-#ht_data_pd.to_json("results/ht_output.json")
-#lt2_data_pd = pd.read_csv("data/DA7_1-L2.csv", header=0)
-#lt2_data_pd.to_json("results/lt2_output.json")
-
-#lt_json_data = pd.read_json("results/lt_output.json")
-#h1_json_data = pd.read_json("results/ht_output.json")
-#lt2_json_data = pd.read_json("results/lt2_output.json")
-
 def convert_data(filename, output_filename):
+    """This function converts the processed data arrays
+    into pandas dataframes and outputs them in json format."""    
     all_data = pd.read_csv(filename, header=0)
     all_data.info()
     all_data.to_json(output_filename)
@@ -112,7 +98,6 @@ def plot():
     lt_input_file = ("DA7_1-L1.csv")
     ht_input_file = ("DA7_1-H1.csv")
     lt2_input_file = ("DA7_1-L2.csv")
-#    plot_file = "thermomag_plot.pdf"
     lt_json_output_file = "lt_data_output.json"
     ht_json_output_file = "ht_data_output.json"
     lt2_json_output_file = "lt2_data_output.json"
@@ -123,18 +108,10 @@ def plot():
     lt_input_filename = os.path.join(data_directory,lt_input_file)
     ht_input_filename = os.path.join(data_directory,ht_input_file)
     lt2_input_filename = os.path.join(data_directory,lt2_input_file)
-#    plot_filename = os.path.join(results_directory,plot_file)
     lt_json_filename = os.path.join(results_directory,lt_json_output_file)
     ht_json_filename = os.path.join(results_directory,ht_json_output_file)
     lt2_json_filename = os.path.join(results_directory,lt2_json_output_file)
     
-#    lt_data_array = read_data(lt_input_filename, starting_row=0)
-#    ht_data_array = read_data(ht_input_filename, starting_row=0)
-#    lt2_data_array = read_data(lt2_input_filename, starting_row=0)
-    
-#    processed_lt_data = process_data(lt_data_array)
-#    processed_ht_data = process_data(ht_data_array)
-#    processed_lt2_data = process_data(lt2_data_array)
     convert_data(lt_input_filename, lt_json_filename)
     convert_data(ht_input_filename, ht_json_filename)
     convert_data(lt2_input_filename, lt2_json_filename)
